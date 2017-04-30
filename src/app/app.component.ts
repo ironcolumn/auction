d650eaf5f5547f7ba8c4cb1ee11af3103d8ba478
zzy <403291934>
2017-04-30 15:22:30 +0800

    auction system

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+// 组件元数据装饰器
+@Component({
+  selector: 'app-root',
+  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'app works!';
+
+}
