a33a92bc668ecf4199d1fd7c1eee20dc48ed6402
zzy <403291934>
2017-05-03 15:58:22 +0800

    auction system

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -19,15 +19,15 @@ export class ProductDetailComponent implements OnInit {
   }
 
   ngOnInit() {
-    const productId: number = this.routeInfo.snapshot.params['productId'];
+    let productId: number = this.routeInfo.snapshot.params['productId'];
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
   }
 
   addComment() {
-    let comment = new Comment(0, this.product.id, new Date().toISOString(), "someone", this.newRating, this.newComment);
+    let comment = new Comment(0, this.product.id, new Date().toISOString(), 'someone', this.newRating, this.newComment);
     this.comments.unshift(comment);
-    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0)
+    const sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
     this.product.rating = sum / this.comments.length;
     this.newComment = null;
     this.newRating = 5;
